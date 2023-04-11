af3271b177506782264e91ce4212941f029c5bff
Brian Heylin <3947+bheylin@users.noreply.github.com>
2023-04-11 11:28:57 +0200

    Rm unused scripts

--- scripts/clippy.sh
+++
@@ -1,7 +0,0 @@
-cargo clippy -- \
-  -D clippy::pedantic \
-  -A clippy::must-use-candidate \
-  -A clippy::doc-markdown \
-  -A clippy::missing-errors-doc \
-  -D clippy::dbg_macro \
-  -D clippy::unwrap_used
